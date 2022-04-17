1e4410a358a542fba3b8f61dab5d4be8ffe299b0
Ohjin Kwon <qpqpq13579@gmail.com>
2022-04-18 06:04:43 +0900

    Change Button, Image style,
    Button: type "outlined" -> "contained",  zIndex +
    Image: Change wrapper size, fixed proportions

--- common/components/gallery/SelectImgModal.tsx
+++ common/components/gallery/SelectImgModal.tsx
@@ -16,8 +16,8 @@ const SelectImgModalWrapper = styled("div")({
   position: "absolute",
   top: "5%",
   left: "5%",
-  height: "94%",
-  width: "90%",
+  height: "88%",
+  width:'90%',
   marginTop: '10px',
   zIndex: 2,
 });
@@ -46,30 +46,30 @@ const SelectImgModal = (): EmotionJSX.Element => {
     <>
       <SelectImgModalContainer>
         <Button
-          sx={{ ml: "5%", mt: "15px" }}
-          variant="outlined"
+          sx={{ ml: "5%", mt: "15px", zIndex:3 }}
+          variant="contained"
           onClick={() => setCurImgIndex(-1)}
         >
           X
         </Button>
-        <Button variant="outlined" sx={{ mt: "15px", mr:'100px', float:"right"}} 
+        <Button variant="contained" sx={{ mt: "15px", mr:'100px', float:"right", zIndex:3}} 
           onClick={()=>handleDel(savedImg, setSavedImg, curImgIndex)}>
           <DeleteIcon></DeleteIcon>
         </Button>
-        <Button variant="outlined" sx={{float:"right", mr:'10px', mt: "15px"}} 
+        <Button variant="contained" sx={{float:"right", mr:'10px', mt: "15px", zIndex:3}} 
           onClick={()=>singleDownload(selectImgUrl)}>
           다운로드
         </Button>
-        <Button variant="outlined" sx={{position: 'fixed', top:'48%', left:'20px', zIndex:3}}
+        <Button variant="contained" sx={{position: 'fixed', top:'48%', left:'20px', zIndex:3}}
           onClick={()=> curImgIndex > 0 && setCurImgIndex(curImgIndex-1)}>
           <ChevronLeftIcon></ChevronLeftIcon>
         </Button>
-        <Button variant="outlined" sx={{position: 'fixed', top:'48%', right:'20px', zIndex:3}}
+        <Button variant="contained" sx={{position: 'fixed', top:'48%', right:'20px', zIndex:3}}
           onClick={()=> curImgIndex < savedImg.length -1 && setCurImgIndex(curImgIndex+1)}>
           <ChevronRightIcon></ChevronRightIcon>
         </Button>
         <SelectImgModalWrapper>
-          {selectImgUrl && <Image layout="fill" unoptimized={true} src={selectImgUrl}></Image>}
+          {selectImgUrl && <Image layout="responsive" height="900" width="1600" unoptimized={true} src={selectImgUrl} ></Image>}
         </SelectImgModalWrapper>
       </SelectImgModalContainer>
     </>
